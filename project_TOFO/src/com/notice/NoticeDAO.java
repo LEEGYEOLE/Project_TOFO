691aa8c51aca07f8456266be35c2ba0b011c94e4
sist <sist@DESKTOP-PFRFCAU>
2019-11-11 16:51:00 +0900

    notice - group마다 공지사항을 올릴 수 있게 했습니다.

--- project_TOFO/src/com/notice/NoticeDAO.java
+++ project_TOFO/src/com/notice/NoticeDAO.java
@@ -5,24 +5,15 @@ import java.util.List;
 public interface NoticeDAO {
 	
 	public int insertNotice(NoticeDTO dto); //글 등록하기
-	public int dataCount(); 
-	public int dataCount(String condition, String keyword);
-	public List<NoticeDTO> listNotice(int offset, int rows); //게시물 리스트 
-	public List<NoticeDTO> listNotice(int offset, int rows, String condition, String keyword);
-	public NoticeDTO readBoard(int num);
-	public List<NoticeDTO> listNotice();
-	public NoticeDTO preReadNotice(int num, String condition, String keyword);  //이전글
-	public NoticeDTO nextReadNotice(int num, String condition, String keyword); //다음글
+	public int dataCount(int groupNum); 
+	public int dataCount(String condition, String keyword , int groupNum);
+	public List<NoticeDTO> listNotice(int offset, int rows,int groupNum); //게시물 리스트 
+	public List<NoticeDTO> listNotice(int offset, int rows, String condition, String keyword, int groupNum);
+	public List<NoticeDTO> listNotice(int groupNum);
+	public NoticeDTO readNotice(int num);
+	public NoticeDTO preReadNotice(int num, String condition, String keyword, int groupNum);  //이전글
+	public NoticeDTO nextReadNotice(int num, String condition, String keyword, int groupNum); //다음글
 	public void updateHitCount(int num);
 	public void updateNotice(NoticeDTO dto); //글 수정하기
 	public void deleteNotice(int num, String userId);  //글 삭제하기
-	public NoticeDTO readNotice(int num);
-	
-	
-	
-	
-	
-	
-	
-
 }
--- project_TOFO/src/com/notice/NoticeDAOlmpl.java
+++ project_TOFO/src/com/notice/NoticeDAOlmpl.java
@@ -21,7 +21,7 @@ public class NoticeDAOlmpl implements NoticeDAO {
 
 		try {
 
-			sql = "INSERT INTO notice(num,  userId, subject, content, saveFilename, originalFilename, filesize, notice) VALUES (notice_seq.NEXTVAL, ?, ?, ?, ?, ?, ?, ?)";
+			sql = "INSERT INTO notice(num,  userId, subject, content, saveFilename, originalFilename, filesize, notice, groupNum) VALUES (notice_seq.NEXTVAL, ?, ?, ?, ?, ?, ?, ?, ?)";
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, dto.getUserId());
 			pstmt.setString(2, dto.getSubject());
@@ -30,7 +30,7 @@ public class NoticeDAOlmpl implements NoticeDAO {
 			pstmt.setString(5, dto.getOriginalFilename());
 			pstmt.setLong(6, dto.getFilesize());
 			pstmt.setInt(7, dto.getNotice());
-
+			pstmt.setInt(8, dto.getGroupNum());
 			pstmt.executeUpdate();
 
 		} catch (Exception e) {
@@ -49,7 +49,7 @@ public class NoticeDAOlmpl implements NoticeDAO {
 	}
 
 	@Override
-	public int dataCount() {
+	public int dataCount(int groupNum) {
 		int result = 0;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
@@ -58,10 +58,12 @@ public class NoticeDAOlmpl implements NoticeDAO {
 
 		try {
 
-			sql = "SELECT NVL(COUNT(*), 0) FROM notice";
+			sql = "SELECT NVL(COUNT(*), 0) FROM notice WHERE groupNum = ?";
 			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, groupNum);
+			
 			rs = pstmt.executeQuery();
-
+			pstmt.setInt(1, groupNum);
 			if (rs.next()) {
 				result = rs.getInt(1);
 
@@ -93,7 +95,7 @@ public class NoticeDAOlmpl implements NoticeDAO {
 	}
 
 	@Override
-	public int dataCount(String condition, String keyword) {
+	public int dataCount(String condition, String keyword , int groupNum) {
 		int result = 0;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
@@ -101,19 +103,20 @@ public class NoticeDAOlmpl implements NoticeDAO {
 
 		try {
 			// 조인안하면 이름 검색이 안된다.
-			sql = "SELECT NVL(COUNT(*), 0) FROM notice b JOIN member m ON b.userId = m.userId"; // 이름을 member이 가지고 있기
+			sql = "SELECT NVL(COUNT(*), 0) FROM notice b JOIN member m ON b.userId = m.userId WHERE grounNum = ? "; // 이름을 member이 가지고 있기
 																									// 때문에
 			if (condition.equalsIgnoreCase("created")) { // created이면~
 				keyword = keyword.replace("-", ""); // condition은 컬럼명
-				sql += " WHERE TO_CHAR(created, 'YYYYMMDD') = ?"; // 날짜검색
+				sql += " AND TO_CHAR(created, 'YYYYMMDD') = ?"; // 날짜검색
 			} else if (condition.equalsIgnoreCase("userName")) {
-				sql += " WHERE INSTR(userName, ?)=1"; // 앞부분쪽 보겠다는 뜻
+				sql += " AND INSTR(userName, ?)=1 "; // 앞부분쪽 보겠다는 뜻
 			} else {
-				sql += " WHERE INSTR(" + condition + ",?) >=1 "; // condition은 컬럼명
+				sql += " AND INSTR(" + condition + ",?) >=1 "; // condition은 컬럼명
 			}
 
 			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, keyword);
+			pstmt.setInt(1, groupNum);
+			pstmt.setString(2, keyword);
 
 			rs = pstmt.executeQuery();
 
@@ -146,7 +149,7 @@ public class NoticeDAOlmpl implements NoticeDAO {
 
 	}
 	@Override
-	public List<NoticeDTO> listNotice(int offset, int rows) {
+	public List<NoticeDTO> listNotice(int offset, int rows, int groupNum) {
 		List<NoticeDTO> list = new ArrayList<NoticeDTO>();
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
@@ -158,84 +161,12 @@ public class NoticeDAOlmpl implements NoticeDAO {
 			sb.append("  	created  ");
 			sb.append(" FROM notice b ");
 			sb.append(" JOIN member m ON b.userId = m.userId  ");
+			sb.append(" WHERE groupNum = ? ");
 			sb.append(" ORDER BY num DESC ");
 			sb.append(" OFFSET ? ROWS FETCH FIRST ? ROWS ONLY  ");
 
 			pstmt = conn.prepareStatement(sb.toString());
-			pstmt.setInt(1, offset);
-			pstmt.setInt(2, rows);
-
-			rs = pstmt.executeQuery();
-
-			while (rs.next()) {
-				NoticeDTO dto = new NoticeDTO();
-				dto.setNum(rs.getInt("num"));
-				dto.setUserName(rs.getString("userName"));
-				dto.setSubject(rs.getString("subject"));
-				dto.setSaveFilename(rs.getString("saveFilename"));
-				dto.setCreated(rs.getString("created"));
-				dto.setHitCount(rs.getInt("hitCount"));
-				list.add(dto);
-
-			}
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			if (rs != null) {
-				try {
-					rs.close();
-				} catch (Exception e2) {
-
-				}
-			}
-
-			if (pstmt != null) {
-				try {
-					pstmt.close();
-				} catch (Exception e2) {
-
-				}
-			}
-
-		}
-
-		return list;
-
-	}
-
-	@Override
-	public List<NoticeDTO> listNotice(int offset, int rows, String condition, String keyword) {
-
-		List<NoticeDTO> list = new ArrayList<NoticeDTO>();
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		StringBuilder sb = new StringBuilder();
-
-		try {
-
-			sb.append("SELECT num, userName, subject, hitCount, saveFilename, ");
-			sb.append("  	created  ");
-			sb.append(" FROM notice b ");
-			sb.append(" JOIN member m ON b.userId = m.userId  ");
-
-			if (condition.equalsIgnoreCase("created")) {
-				keyword = keyword.replace("-", ""); // condition은 컬럼명
-				sb.append("  WHERE TO_CHAR(created, 'YYYYMMDD') = ?  ");
-				{
-				}
-
-			} else if (condition.equalsIgnoreCase("userName")) {
-				sb.append("WHERE INSTR(userName, ?)=1 "); // 앞부분쪽 보겠다는 뜻
-			} else {
-				sb.append("WHERE INSTR(" + condition + ",?) >=1 ");// condition은 컬럼명
-			}
-
-			sb.append(" ORDER BY num DESC ");
-			sb.append(" OFFSET ? ROWS FETCH FIRST ? ROWS ONLY  ");
-
-			pstmt = conn.prepareStatement(sb.toString());
-			pstmt.setString(1, keyword);
+			pstmt.setInt(1, groupNum);
 			pstmt.setInt(2, offset);
 			pstmt.setInt(3, rows);
 
@@ -249,6 +180,80 @@ public class NoticeDAOlmpl implements NoticeDAO {
 				dto.setSaveFilename(rs.getString("saveFilename"));
 				dto.setCreated(rs.getString("created"));
 				dto.setHitCount(rs.getInt("hitCount"));
+				list.add(dto);
+
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (Exception e2) {
+
+				}
+			}
+
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (Exception e2) {
+
+				}
+			}
+
+		}
+
+		return list;
+
+	}
+
+	@Override
+	public List<NoticeDTO> listNotice(int offset, int rows, String condition, String keyword , int groupNum) {
+
+		List<NoticeDTO> list = new ArrayList<NoticeDTO>();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		StringBuilder sb = new StringBuilder();
+
+		try {
+
+			sb.append("SELECT num, userName, subject, hitCount, saveFilename, ");
+			sb.append("  	created  ");
+			sb.append(" FROM notice b ");
+			sb.append(" JOIN member m ON b.userId = m.userId  ");
+			sb.append(" WHERE groupNum = ? ");
+
+			if (condition.equalsIgnoreCase("created")) {
+				keyword = keyword.replace("-", ""); // condition은 컬럼명
+				sb.append("  AND TO_CHAR(created, 'YYYYMMDD') = ?  ");
+				
+			} else if (condition.equalsIgnoreCase("userName")) {
+				sb.append(" AND INSTR(userName, ?)=1 "); // 앞부분쪽 보겠다는 뜻
+			} else {
+				sb.append(" AND INSTR(" + condition + ",?) >=1 ");// condition은 컬럼명
+			} 
+
+			sb.append(" ORDER BY num DESC ");
+			sb.append(" OFFSET ? ROWS FETCH FIRST ? ROWS ONLY  ");
+
+			pstmt = conn.prepareStatement(sb.toString());
+			pstmt.setInt(1, groupNum);
+			pstmt.setString(2, keyword);
+			pstmt.setInt(3, offset);
+			pstmt.setInt(4, rows);
+
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				NoticeDTO dto = new NoticeDTO();
+				dto.setNum(rs.getInt("num"));
+				dto.setUserName(rs.getString("userName"));
+				dto.setSubject(rs.getString("subject"));
+				dto.setSaveFilename(rs.getString("saveFilename"));
+				dto.setCreated(rs.getString("created"));
+				dto.setHitCount(rs.getInt("hitCount"));
 
 				list.add(dto);
 			}
@@ -279,57 +284,7 @@ public class NoticeDAOlmpl implements NoticeDAO {
 	}
 
 	@Override
-	public NoticeDTO readBoard(int num) {
-		NoticeDTO dto = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-
-		String sql;
-
-		try {
-
-			sql = "SELECT num , b.userId , userName, subject, content, created, hitCount FROM notice b JOIN member m ON b.userId=m.userId WHERE num= ?";
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, num);
-			rs = pstmt.executeQuery();
-
-			if (rs.next()) {
-				dto = new NoticeDTO();
-				dto.setNum(rs.getInt("num"));
-				dto.setUserId(rs.getString("userId"));
-				dto.setUserName(rs.getString("userName"));
-				dto.setSubject(rs.getString("subject"));
-				dto.setContent(rs.getString("content"));
-				dto.setCreated(rs.getString("created"));
-				dto.setHitCount(rs.getInt("hitCount"));
-
-			}
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			if (rs != null) {
-				try {
-					rs.close();
-				} catch (Exception e2) {
-
-				}
-			}
-
-			if (pstmt != null) {
-				try {
-					pstmt.close();
-				} catch (Exception e2) {
-
-				}
-			}
-		}
-
-		return dto;
-	}
-
-	@Override
-	public List<NoticeDTO> listNotice() {
+	public List<NoticeDTO> listNotice(int groupNum) {
 		List<NoticeDTO> list = new ArrayList<NoticeDTO>();
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
@@ -339,11 +294,11 @@ public class NoticeDAOlmpl implements NoticeDAO {
 			sb.append("SELECT num, userName, subject, saveFilename,  ");
 			sb.append("       hitCount, TO_CHAR(created, 'YYYY-MM-DD') created  ");
 			sb.append(" FROM notice n JOIN member m ON n.userId=m.userId  ");
-			sb.append(" WHERE notice=1  ");
+			sb.append(" WHERE notice=1 AND groupNum=? ");
 			sb.append(" ORDER BY num DESC ");
 
 			pstmt = conn.prepareStatement(sb.toString());
-
+			pstmt.setInt(1, groupNum);
 			rs = pstmt.executeQuery();
 
 			while (rs.next()) {
@@ -380,12 +335,66 @@ public class NoticeDAOlmpl implements NoticeDAO {
 
 	}
 	
-	
-	
+	@Override
+	public NoticeDTO readNotice(int num) {
+		NoticeDTO dto = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		StringBuffer sb = new StringBuffer();
+
+		// 컬럼 다 나열함
+		try {
+			sb.append("SELECT num, n.userId, userName, subject, saveFilename,  originalFilename, ");
+			sb.append("    content, hitCount, created , fileSize, notice  ");
+			sb.append(" FROM notice n JOIN member m ON n.userId=m.userId  ");
+			sb.append(" WHERE num=?  ");
+
+			pstmt = conn.prepareStatement(sb.toString());
+			pstmt.setInt(1, num);
+
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				dto = new NoticeDTO();
+
+				dto.setNum(rs.getInt("num"));
+				dto.setUserId(rs.getString("userId"));
+				dto.setUserName(rs.getString("userName"));
+				dto.setSubject(rs.getString("subject"));
+				dto.setContent(rs.getString("content"));
+				dto.setSaveFilename(rs.getString("saveFilename"));
+				dto.setOriginalFilename(rs.getString("originalFilename"));
+				dto.setHitCount(rs.getInt("hitCount"));
+				dto.setCreated(rs.getString("created"));
+				dto.setFilesize(rs.getInt("filesize"));
+				dto.setNotice(rs.getInt("notice"));
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+				}
+			}
+
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+
+		return dto;
+
+	}
 	
 
 	@Override
-	public NoticeDTO preReadNotice(int num, String condition, String keyword) {
+	public NoticeDTO preReadNotice(int num, String condition, String keyword, int groupNum) {
 		NoticeDTO dto = null;
 
 		PreparedStatement pstmt = null;
@@ -395,31 +404,34 @@ public class NoticeDAOlmpl implements NoticeDAO {
 		try {
 			if (keyword != null && keyword.length() != 0) {
 				sb.append("SELECT ROWNUM, tb.* FROM ( ");
-				sb.append("  SELECT num, subject FROM notice b JOIN member m ON b.userId = m.userId ");
+				sb.append("  SELECT num, subject FROM notice b JOIN member m ON b.userId = m.userId AND groupNum=? ");
 				 if(condition.equalsIgnoreCase("created")) {
 	                   keyword=keyword.replaceAll("-", "");
-	                   sb.append(" WHERE (TO_CHAR(created, 'YYYYMMDD') = ?)  ");
+	                   sb.append(" AND (TO_CHAR(created, 'YYYYMMDD') = ?)  ");
 	                } else {
-	                   sb.append(" WHERE (INSTR(" + condition + ", ?) >= 1)  ");
+	                   sb.append(" AND (INSTR(" + condition + ", ?) >= 1)  ");
 	                }
-	                sb.append("         AND (num > ? )  ");
+	                sb.append("         AND (num > ? ) ");
 	                sb.append(" ORDER BY num ASC  ");
 	                sb.append(" FETCH  FIRST  1  ROWS  ONLY");
 
 				pstmt = conn.prepareStatement(sb.toString());
-				pstmt.setString(1, keyword);
-				pstmt.setInt(2, num);
+				pstmt.setInt(1, groupNum);
+				pstmt.setString(2, keyword);
+				pstmt.setInt(3, num);
+
 
 			} else {
 				sb.append("SELECT ROWNUM, tb.* FROM ( ");
 				sb.append("  SELECT num, subject FROM notice "); // 이건 조인안해도됨 , 이건 이름 검색을 안해도 되기 때문에
 																	// 해도 되고 안해도 됨
-				sb.append("     WHERE num > ? ");
+				sb.append("     WHERE num > ? AND groupNum =? ");
 				sb.append("         ORDER BY num ASC ");
 				sb.append("      ) tb WHERE ROWNUM=1 ");
 
 				pstmt = conn.prepareStatement(sb.toString());
 				pstmt.setInt(1, num);
+				pstmt.setInt(2, groupNum);
 			}
 
 			rs = pstmt.executeQuery();
@@ -450,7 +462,7 @@ public class NoticeDAOlmpl implements NoticeDAO {
 		return dto;
 	}
 	@Override
-	public NoticeDTO nextReadNotice(int num, String condition, String keyword) {
+	public NoticeDTO nextReadNotice(int num, String condition, String keyword, int groupNum) {
 		NoticeDTO dto = null;
 
 		PreparedStatement pstmt = null;
@@ -460,30 +472,32 @@ public class NoticeDAOlmpl implements NoticeDAO {
 		try {
 			if (keyword != null && keyword.length() != 0) {
 				sb.append("SELECT ROWNUM, tb.* FROM ( ");
-				sb.append("  SELECT num, subject FROM notice b JOIN member m ON b.userId = m.userId ");
+				sb.append("  SELECT num, subject FROM notice b JOIN member m ON b.userId = m.userId  WHERE groupNum = ? ");
 				 if(condition.equalsIgnoreCase("created")) {
 	                   keyword=keyword.replaceAll("-", "");
-	                   sb.append(" WHERE (TO_CHAR(created, 'YYYYMMDD') = ?)  ");
+	                   sb.append(" AND (TO_CHAR(created, 'YYYYMMDD') = ?)  ");
 	                } else {
-	                   sb.append(" WHERE (INSTR(" + condition + ", ?) >= 1)  ");
+	                   sb.append(" AND (INSTR(" + condition + ", ?) >= 1)  ");
 	                }
 	                sb.append("         AND (num < ? )  ");
 	                sb.append(" ORDER BY num DESC  ");
 	                sb.append(" FETCH  FIRST  1  ROWS  ONLY");
 
 				pstmt = conn.prepareStatement(sb.toString());
-				pstmt.setString(1, keyword);
-				pstmt.setInt(2, num);
+				pstmt.setInt(1, groupNum);
+				pstmt.setString(2, keyword);
+				pstmt.setInt(3, num);
 
 			} else {
 				sb.append("SELECT ROWNUM, tb.* FROM ( ");
 				sb.append("  SELECT num, subject FROM notice ");
-				sb.append("     WHERE num < ? ");
+				sb.append("     WHERE groupNum = ? AND num < ? ");
 				sb.append("         ORDER BY num DESC ");
 				sb.append("      ) tb WHERE ROWNUM=1 ");
 
 				pstmt = conn.prepareStatement(sb.toString());
-				pstmt.setInt(1, num);
+				pstmt.setInt(1, groupNum);
+				pstmt.setInt(2, num);
 			}
 
 			rs = pstmt.executeQuery();
@@ -605,63 +619,6 @@ public class NoticeDAOlmpl implements NoticeDAO {
 				}
 			}
 		}
-	}
-
-	@Override
-	public NoticeDTO readNotice(int num) {
-		NoticeDTO dto = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		StringBuffer sb = new StringBuffer();
-
-		// 컬럼 다 나열함
-		try {
-			sb.append("SELECT num, n.userId, userName, subject, saveFilename,  originalFilename, ");
-			sb.append("    content, hitCount, created , fileSize, notice  ");
-			sb.append(" FROM notice n JOIN member m ON n.userId=m.userId  ");
-			sb.append(" WHERE num=?  ");
-
-			pstmt = conn.prepareStatement(sb.toString());
-			pstmt.setInt(1, num);
-
-			rs = pstmt.executeQuery();
-
-			if (rs.next()) {
-				dto = new NoticeDTO();
-
-				dto.setNum(rs.getInt("num"));
-				dto.setUserId(rs.getString("userId"));
-				dto.setUserName(rs.getString("userName"));
-				dto.setSubject(rs.getString("subject"));
-				dto.setContent(rs.getString("content"));
-				dto.setSaveFilename(rs.getString("saveFilename"));
-				dto.setOriginalFilename(rs.getString("originalFilename"));
-				dto.setHitCount(rs.getInt("hitCount"));
-				dto.setCreated(rs.getString("created"));
-				dto.setFilesize(rs.getInt("filesize"));
-				dto.setNotice(rs.getInt("notice"));
-
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			if (rs != null) {
-				try {
-					rs.close();
-				} catch (SQLException e) {
-				}
-			}
-
-			if (pstmt != null) {
-				try {
-					pstmt.close();
-				} catch (SQLException e) {
-				}
-			}
-		}
-
-		return dto;
-
 	}
 
 }
